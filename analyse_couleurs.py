b872829e079265b6af1c3f20c7d8585356d93ca5
Alteor13 <martin.charrondiere@cpe.fr>
2024-03-18 09:45:50 +0100

    Push test

---
+++ analyse_couleurs.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy
+import math
+import matplotlib.pyplot as plot
+from sklearn.cluster import KMeans
+
+imgfile = Image.open("images/images/jynx.png")
+numarray = numpy.array(imgfile.getdata(), numpy.uint8)
+clusters = KMeans(n_clusters=4, n_init=2)
+clusters.fit(numarray)
+npbins = numpy.arange(0, 5)
+histogram = numpy.histogram(clusters.labels_, bins=npbins)
+labels = numpy.unique(clusters.labels_)
+barlist = plot.bar(labels, histogram[0])
+for i in range(4):
+    barlist[i].set_color(
+        "#%02x%02x%02x"
+        % (
+            math.ceil(clusters.cluster_centers_[i][0]),
+            math.ceil(clusters.cluster_centers_[i][1]),
+            math.ceil(clusters.cluster_centers_[i][2]),
+        )
+    )
+plot.show()
